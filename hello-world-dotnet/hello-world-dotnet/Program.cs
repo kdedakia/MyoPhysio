a62fe34566381c4f578746cdece54f8313646ba2
Vishal Mathur <vishmathur5@gmail.com>
2013-09-13 23:05:26 -0400

    added duration and angular velocity methods

--- hello-world-dotnet/hello-world-dotnet/Program.cs
+++ hello-world-dotnet/hello-world-dotnet/Program.cs
@@ -11,6 +11,14 @@ namespace hello_world_dotnet
             Hub hub = new Hub();
             SimpleDeviceListener deviceListener = new SimpleDeviceListener();
 
+            SimpleDeviceListener motionDataListener = new SimpleDeviceListener();
+
+            motionDataListener.PoseStart += (object sender, PoseEventArgs e) => 
+            { 
+                Quaternion quat;
+                quat = hub.DefaultDevice.Motion.Orientation;
+            };
+
             //Step 2: Hook up the handers for the events you care about
             deviceListener.PoseStart += (object sender, PoseEventArgs e) => 
             {
@@ -30,10 +38,21 @@ namespace hello_world_dotnet
 
             //Step 3: Add the listener to the default device
             hub.DefaultDevice.AddListener(deviceListener);
-
+            hub.DefaultDevice.AddListener(motionDataListener);
             Console.WriteLine("Round 1: FIGHT");
             Console.WriteLine("Press enter to Quit");
             Console.ReadLine();
         }
+
+        public static double getDuration(int timeStart, int timeFinish)
+        {
+            return timeFinish-timeStart;
+        }
+
+        public static double angularVelocityMagnitude(int x, int y, int z)
+        {
+            return Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2) + Math.Pow(z, 2));
+        }
     }
+ 
 }
