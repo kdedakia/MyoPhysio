8e101370268dca4278420c8fd7e63d42ddbb5685
Samuel Yuan <sam.yuan.ca@gmail.com>
2013-09-14 05:43:53 -0400

    Model/view, with basic console logic to test.

---
+++ hello-world-cpp/hello-world-cpp/console.cpp
@@ -0,0 +1,6 @@
+#include "console.h"
+
+void Console::update(Model *model) {
+	Move move = model->lastMove;
+	std::cout << move.yaw << "," << move.pitch << "," << move.roll << std::endl;
+}
---
+++ hello-world-cpp/hello-world-cpp/move.cpp
@@ -0,0 +1,9 @@
+#include "move.h"
+
+Move::Move(double yaw, double pitch, double roll, double aX, double aY, double aZ,
+		double wX, double wY, double wZ) {
+	yaw = yaw;
+	this.pitch = pitch;
+	this.roll = roll;
+	this.aX = aX;
+}
